5d3bdecf403f942d33553e55e660391d2d278edb
Mathew Liew <mliew21396@gmail.com>
2015-09-13 12:11:05 -0500

    Fix deck minor errors

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -6,10 +6,12 @@ end
 # Game logic
 get '/decks/:id' do
 
+  redirect :"login" unless session[:user_id]
   # Must change user Id
   @game = Game.create(user_id: session[:user_id], deck_id: params[:id])
 
   redirect :"/games/#{@game.id}"
+
 
   # @deck = Deck.find(params[:id])
   # @shuffled_cards = @deck.cards.shuffle
