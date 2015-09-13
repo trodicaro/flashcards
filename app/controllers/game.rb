33480157b004ba9ac6445753afc77e04ebed443a
Paulo Gonzalez & Rodica Trifan <pair+pdgonzalez872.trodicaro@devbootcamp.com>
2015-09-12 19:55:27 -0500

    Playing around with the logic

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -1,7 +1,10 @@
 get '/games/:id' do
-# conditional
-  # if
+
+
   @game = Game.find(params[:id])
+  logger.info "THIS IS THE GAME: #{@game} #{@game.id} #{@game.deck_id}"
+  logger.info "Game deck: #{@game.deck}"
+  logger.info "Game cards: #{@game.deck.cards}"
   @cards = @game.deck.cards
 
   p "This is the game"
@@ -11,59 +14,46 @@ get '/games/:id' do
   p "This are the cards"
   logger.info @cards
 
-  # iterate EACH/WHILE
-
-  # @game.card_solved?()
-
-
+  game_won = false
 
   @cards.each do |card|
 
-    if @game.guesses.empty?
-      logger.info "THIS IS EMPTY!!!!!!!"
-      guess = Guess.create(card_id: card.id, game_id: @game.id, solved: false)
-    end
+    # This may or may not need to be here....
+    # if @game.guesses.empty?
+    #   logger.info "THIS IS EMPTY!!!!!!!"
+    #   guess = Guess.create(card_id: card.id, game_id: @game.id, solved: false)
+    # end
 
-    # current_game_guesses = card.guesses.where(game_id: @game.id)
-
-    # if card.guesses.where(game_id: @game.id)[-1]["solved"] == true
-    # if @game.guesses[-1]
-
-    logger.info "@game.guesses.where(card_id: card.id) -> #{@game.guesses.where(card_id: card.id)}"
-    logger.info "@game.guesses -> #{@game.guesses}"
-    logger.info "card.id -> #{card.id}"
-    logger.info "@game.guesses.where(card_id: card.id).pluck(:solved) -> #{@game.guesses.where(card_id: card.id).pluck(:solved)}"
-
-    # logger.info "@game.guesses.to_a -> #{@game.guesses.to_a}"
-
-
-    # logger.info "game_guesses.pluck(:solved)-> #{game_guesses.pluck(:solved)}"
-
-    # if @game.guesses.where(card_id: card.id)[-1]["solved"] == true
     if @game.guesses.where(card_id: card.id).pluck(:solved).include?(true)
-
-      # logger.info
-
-
-    # if card.guesses[-1]["solved"] == true
-    # irb(main):019:0> cards[0].guesses.where(game_id: 39)
       next
     else
       @current_card = card
       logger.info card
-      break
-      # show the card page
-      # erb :"cards/show"
+      # game_won = true
+      logger.info "game_won -> #{game_won}"
+      # break
+      return erb :"cards/show"
     end
-
     # erb :"cards/show"
+    logger.info "WE ARE ABOVE GAMEWON"
+    # game_won = true
   end
 
   logger.info "YOU JUST BEAT THE GAMEEEEEEE"
 
-  # game end page
-  erb :"cards/show"
+  return redirect :"decks/index"
+  # logger.info "game_won -> #{game_won}"
+  # if game_won == true
+  #   redirect :"/decks/index"
+  # else
+  #   erb :"cards/show"
+  # end
 
+
+  # erb :"cards/show"
+  # redirect :"/decks/index"
+
+  # redirect congratulations!!!!
 end
 
 # maybe not restful, but ok for now
